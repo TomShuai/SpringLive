38abb8f57f2987f9266b512050b1c9263c871581
Tomshuai <807505515@qq.com>
2017-04-10 11:20:08 +0800

    main commit

--- app/src/main/java/beicai/com/jiangshuai/springlive/MainActivity.java
+++ app/src/main/java/beicai/com/jiangshuai/springlive/MainActivity.java
@@ -2,6 +2,7 @@ package beicai.com.jiangshuai.springlive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"22222",Toast.LENGTH_LONG).show();
     }
 }
