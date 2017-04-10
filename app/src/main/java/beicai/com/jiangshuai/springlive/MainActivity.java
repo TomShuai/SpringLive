b33a9cabdd7d4f4d12f02be2e333d17bf7243a7f
Tomshuai <807505515@qq.com>
2017-04-10 14:45:49 +0800

    什么鬼

--- app/src/main/java/beicai/com/jiangshuai/springlive/MainActivity.java
+++ app/src/main/java/beicai/com/jiangshuai/springlive/MainActivity.java
@@ -2,6 +2,7 @@ package beicai.com.jiangshuai.springlive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
